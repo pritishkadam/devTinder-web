056e60d15320a404318e6b2110ab8d9d9592f673
Pritish Kadam <prtshkdm18@gmail.com>
2024-12-18 23:22:34 +0530

    initial commit

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import NavBar from './NavBar';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
+
+const Body = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Body;
